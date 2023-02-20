68696f8045cbd99edfca4d7e44af05cd07456fe5
Nick <ncalfano35@gmail.com>
2023-02-19 21:16:56 -0500

    Plus 1 Health
    Changed the health multiplier to just be an addition of 1 health point instead of doubling the health.
    TLDR 3 -> 4 instead of 3 -> 6

--- KeyBrawl/Assets/Scripts/HealthPowerup.cs
+++ KeyBrawl/Assets/Scripts/HealthPowerup.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HealthPowerup : MonoBehaviour
 {
-    public int multiplier = 2;
+    public int add = 1;
     public float duration = 1000f;
 
     void OnTriggerEnter2D(Collider2D other)
@@ -18,13 +18,13 @@ public class HealthPowerup : MonoBehaviour
     IEnumerator Pickup(Collider2D player)
     {
         PlayerController1 stats = player.GetComponent<PlayerController1>();
-        stats.health *= multiplier;
+        stats.health += add;
 
         GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<Collider2D>().enabled = false;
 
         yield return new WaitForSeconds(duration);
-        stats.health /= multiplier;
+        stats.health /= add;
         Destroy(gameObject);
     }
 
